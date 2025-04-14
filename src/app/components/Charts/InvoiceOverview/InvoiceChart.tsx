4af00fc407a91719c8f05b071227c5005a72a7b0
Shehzain Hassan <shehzainhassan@gmail.com>
2025-04-14 16:59:36 +0500

    Updated Invoice Chart Styling

--- src/app/components/Charts/InvoiceOverview/InvoiceChart.tsx
+++ src/app/components/Charts/InvoiceOverview/InvoiceChart.tsx
@@ -8,10 +8,69 @@ import {
   Tooltip,
   XAxis,
   YAxis,
+  XAxisProps,
+  YAxisProps,
 } from "recharts";
 import { InvoiceChartProps } from "../../../../../Interfaces";
+import {
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
+import {
+  CustomTooltipContainer,
+  CustomTooltipLabel,
+  CustomTooltipValue,
+} from "../BalanceOverTime/BalanceTime";
+
+type TickProps = {
+  x: number;
+  y: number;
+  payload: {
+    value: string | number;
+    coordinate: number;
+  };
+};
 
 export default function InvoiceChart({ invoiceData }: InvoiceChartProps) {
+  const CustomXAxisTick = ({ x, y, payload }: TickProps) => (
+    <text
+      x={x}
+      y={y + 10}
+      textAnchor="middle"
+      fontSize={12}
+      fontWeight={500}
+      fill={BFMPalette.black100}>
+      {payload.value}
+    </text>
+  );
+  const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <CustomTooltipContainer>
+          <CustomTooltipLabel>{label}</CustomTooltipLabel>
+          {payload.map((entry: any, index: number) => (
+            <CustomTooltipValue key={index}>
+              {entry.name}: {Number(entry.value).toLocaleString()}
+            </CustomTooltipValue>
+          ))}
+        </CustomTooltipContainer>
+      );
+    }
+
+    return null;
+  };
+  const CustomYAxisTick = ({ x, y, payload }: TickProps) => (
+    <text
+      x={x - 10}
+      y={y + 4}
+      textAnchor="end"
+      fontSize={12}
+      fontWeight={400}
+      fill={BFMPalette.black100}>
+      {Number(payload.value).toLocaleString()}
+    </text>
+  );
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart
@@ -20,12 +79,21 @@ export default function InvoiceChart({ invoiceData }: InvoiceChartProps) {
         <CartesianGrid
           stroke={BFMPalette.gray100}
           strokeWidth={1}
-          horizontal={true}
+          horizontal
           vertical={false}
         />
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
+        <XAxis
+          dataKey="month"
+          axisLine={false}
+          tickLine={false}
+          tick={(props) => <CustomXAxisTick {...props} />}
+        />
+        <YAxis
+          axisLine={false}
+          tickLine={false}
+          tick={(props) => <CustomYAxisTick {...props} />}
+        />
+        <Tooltip content={<CustomTooltip />} />
         <Bar
           dataKey="value1"
           fill={BFMPalette.purple200}
